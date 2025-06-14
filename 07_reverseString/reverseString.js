f0d430f31916a979accaab1cf416a27bccb141b1
Golla Bharath <gollabharath2007@gmail.com>
2025-06-14 14:16:48 +0530

    Got carried away lol

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    res = ""
+    for (let i = 0;i<(string.length);i++) {
+        res += string[string.length-i-1]
+    }
+    return res
 };
 
 // Do not edit below this line
