def49ad8e6603fd70683f49462976f999994d717
Golla Bharath <gollabharath2007@gmail.com>
2025-06-14 20:07:49 +0530

    getting through this

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let res = []
+    for (x of arr) {
+        if (args.includes(x)){}
+        else {
+            res.push(x)
+        }
+    }
+    return res
 };
 
 // Do not edit below this line
